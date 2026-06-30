289f2e8e56cc8b5b287b67781b714defc4f91d23
bw.xie <bw.xie@tec-do.com>
2026-06-30 14:26:50 +0800

    chore: 调整 Apollo 默认集群与异常日志

--- services/live-stream/core/apollo/__init__.py
+++ services/live-stream/core/apollo/__init__.py
@@ -6,7 +6,7 @@ from core.apollo.apollo_client import ApolloClient
 # Apollo 引导参数（唯一允许走环境变量的配置，鸡生蛋问题）
 apollo_id = os.environ.get("APOLLOID", "live-spider")
 config_url = os.environ.get("APOLLO_URL", "http://dev-apollo.tec-develop.com")
-cluster = os.environ.get("DEPLOY_ENV", "dev01")
+cluster = os.environ.get("DEPLOY_ENV", "dev02")
 
 
 APOLLO = ApolloClient(
